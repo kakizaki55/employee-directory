f7e4e4e0a68a8ac185b82956e3b71d68898e9c5c
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-02-25 17:22:57 -0800

    added secrets

--- src/views/CreateEditProfile/CreateEditProfile.jsx
+++ src/views/CreateEditProfile/CreateEditProfile.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import CreateEditProfileForm from '../../components/CreateEditProfileForm/CreateEditProfileForm';
-import { useProfile } from '../../context/ProfileContext';
 
 export default function CreateEditProfile({ isEditing }) {
   return (
